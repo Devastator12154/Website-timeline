f72c04bd6d0f7d4505510a6ec089f502ac2cc0b7
Imperium1 <stvstroud0123@gmail.com>
2024-02-27 17:56:40 -0600

    Updates!!!

--- movie_backend/movies_auth/serializers.py
+++ movie_backend/movies_auth/serializers.py
@@ -10,7 +10,7 @@ class myTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = super(myTokenObtainPairSerializer, cls).get_token(user)
         token['username'] = user.username
         return token
-class registerserializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True,validators=[UniqueValidator(queryset=User.objects.all())])
     password = serializers.CharField(write_only=True,required=True,validators=[validate_password])
     password2 = serializers.CharField(write_only=True,required=True)
@@ -22,4 +22,9 @@ class registerserializer(serializers.ModelSerializer):
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({'password':'Passwords do not match'})
         return attrs
+    def create (self, validated_data):
+        user = User.objects.create(username=validated_data['username'],email=validated_data['email'],first_name=validated_data['firstname'],last_name=validated_data['lastname'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
     
